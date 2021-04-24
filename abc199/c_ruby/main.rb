c8715352933f561d8ea8f9ac34fb1510caab61ac
Kenji Koshikawa <koshikawa2009@gmail.com>
2021-04-25 01:59:40 +0900

    solve abc199/c

--- abc199/c_ruby/main.rb
+++ abc199/c_ruby/main.rb
@@ -1,35 +1,27 @@
 # https://atcoder.jp/contests/abc199/tasks/abc199_c
 
-def do_query(n, str, query)
-  t, a, b = query
-  if t == 1
-    c1 = str[a - 1]
-    c2 = str[b - 1]
-    str[a - 1] = c2
-    str[b - 1] = c1
-    str
-  else
-    str[n, n] + str[0, n]
-  end
-end
-# puts do_query(3, 'abcdef', [1, 3, 5])
-# puts do_query(3, 'abcdef', [2, 2, 2])
-
 def main
   n = gets.chomp.to_i
   s = gets.chomp
   q = gets.chomp.to_i
-  # puts "N: #{n}"
-  # puts "S: #{s}"
-  # puts "Q: #{q}"
-
-  # puts "[0] #{s}"
+  is_switched = false
   1.upto q do |_i|
-    query = gets.chomp.split(' ').map(&:to_i)
-    s = do_query(n, s, query)
-    # puts "[#{_i}] #{s}"
+    t, a, b = gets.chomp.split(' ').map(&:to_i)
+    if t == 1
+      a_index = a - 1
+      b_index = b - 1
+      if is_switched
+        a_index = a > n ? a_index - n : a_index + n
+        b_index = b > n ? b_index - n : b_index + n
+      end
+      tmp = s[a_index]
+      s[a_index] = s[b_index]
+      s[b_index] = tmp
+    else
+      is_switched = !is_switched
+    end
   end
-
+  s = s[n, n] + s[0, n] if is_switched
   print s
 end
 
