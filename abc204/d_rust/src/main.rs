f1fba91f8ef4801ca9b46ad042e711404b77ee37
Kenji Koshikawa <koshikawa2009@gmail.com>
2021-06-13 01:08:51 +0900

    improved the answer of abc204-D.

--- abc204/d_rust/src/main.rs
+++ abc204/d_rust/src/main.rs
@@ -1,16 +1,7 @@
-// https://atcoder.jp/contests/abc204/tasks/abc204_d
-// 公式解説を元に実装
-//
-// <制約>
-// N <= 10**2
-// ti <= 10**3
-// Total <= 10**5
-//
-// 解法:部分和問題に帰着させる
-// - 計算量: O(N x T) => 10**7
-//
-// Σb = Total - Σa
-// で一位に決まる。
+// v1の改良版: 以下を参考に改良
+// https://note.com/momomo0214/n/n18a3c1872aff
+// 2次元配列から1次元にして、メモリ・実行時間を効率化。
+
 
 fn main() {
     proconio::input! {
@@ -18,25 +9,20 @@ fn main() {
         t: [usize; n]
     }
     let total = t.iter().sum::<usize>();
-    // dp[i][j]: 片方のオーブンAにおいて、料理i個目までの調理時間が j分にすることができるか。
-    let mut dp = vec![vec![false; total+1]; n+1];
-    dp[0][0] = true;
+    // dp[j]: 片方のオーブンAにおいて、最後の料理までの調理時間が j分にすることができるか。
+    let mut dp = vec![false; total+1];
+    dp[0] = true;
     for i in 1..=n {
-        for j in 0..=total {
-            if dp[i-1][j] {
-                // 一つ前の料理iがJ分だったものがあればDP更新
-                // 更新1: オーブンAで調理しない
-                dp[i][j] = true;
-                // 更新2: オーブンAで調理する (調理時間追加)
-                dp[i][j + t[i-1]] = true;
+        // 調理時間が大きい方からDP更新を行うように変更
+        for j in (0..=total).rev() {
+            if dp[j] {
+                dp[j + t[i-1]] = true;
             }
         }
     }
     let mut ans = total;
-    // 理論上のアンサーの最小値となりうる total/2 から探索
     for j in (total/2)..=total {
-        if dp[n][j] {
-            // オーブンAとオーブンBの最大の調理時間が小さい方で最小値を比較してAnswerを更新
+        if dp[j] {
             ans = ans.min(j.max(total-j));
         }
     }
