1f4056724b0551730490faaa7309241de822f498
Kenji Koshikawa <koshikawa2009@gmail.com>
2021-08-10 01:13:56 +0900

    solved 213-D.

--- abc213/d_rust/src/main.rs
+++ abc213/d_rust/src/main.rs
@@ -1,27 +1,37 @@
-use std::cmp::Reverse;
-use std::collections::BinaryHeap;
-
-fn main() {
-    proconio::input! { q: usize }
-    let mut heap = BinaryHeap::new();
-    let mut add: i64 = 0;
-    for _ in 0..q {
-        proconio::input! { p: usize }
-        match p {
-            1 => {
-                proconio::input! { x: i64 }
-                heap.push(Reverse(x - add));
-            }
-            2 => {
-                proconio::input! { x: i64 }
-                add += x;
-            }
-            3 => {
-                if let Some(Reverse(v)) = heap.pop() {
-                    println!("{}", v + add);
-                }
-            }
-            _ => unreachable!(),
+fn dfs(
+    city: usize,
+    edges_info: &Vec<Vec<usize>>,
+    visited_info: &mut Vec<bool>,
+    histories: &mut Vec<usize>,
+) {
+    visited_info[city] = true;
+    histories.push(city + 1);
+    let edges = edges_info.get(city).unwrap();
+    for next_city in edges {
+        if !visited_info.get(*next_city).unwrap() {
+            dfs(*next_city, edges_info, visited_info, histories);
+            histories.push(city + 1);
         }
     }
+}
+
+fn main() {
+    proconio::input! {
+        n: usize,
+        edges: [(usize, usize); n-1],
+    }
+    let mut edges_each_city = vec![vec![]; n];
+    for (a, b) in edges {
+        edges_each_city[a - 1].push(b - 1);
+        edges_each_city[b - 1].push(a - 1);
+    }
+    for i in 0..n {
+        edges_each_city[i].sort();
+    }
+    let mut hisitories = vec![];
+    let mut visited_info = vec![false; n];
+    dfs(0, &edges_each_city, &mut visited_info, &mut hisitories);
+
+    let ans: Vec<_> = hisitories.iter().map(|x| x.to_string()).collect();
+    println!("{}", ans.join(" "));
 }
